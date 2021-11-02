287a42917c82bc34896c93737e8de5e6687f5adf
Justinking1990 <kingjust@iu.edu>
2021-11-02 09:57:50 -0400

    changed what thumbnail will show

--- commands/changelog.js
+++ commands/changelog.js
@@ -9,13 +9,12 @@ module.exports = {
         .setDescription('Displays the current changelog message to show my users what\'s new'),
     async execute(client, message, args, Discord, interaction) {
         const changelogMessage = require('../changelog.json');
-        const attachment = new Discord
-                      .MessageAttachment('../Images/changelog.gif', 'changelog.gif');
+        const attachment = new Discord.MessageAttachment('../');
         const messageEmbed = new MessageEmbed()
         .setColor("AQUA")
         .setTitle("Change Log")
         .setDescription(`${changelogMessage.text}`)
-        .setThumbnail(attachment({dynamic: true}))
+        .setThumbnail(message.author.displayAvatar({dynamic: true}))
         .setFooter(changelogMessage.footer);
         message.channel.send(messageEmbed)
     },
