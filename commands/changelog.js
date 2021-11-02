3871aae28b62ce36460450248552b421786862db
Justinking1990 <kingjust@iu.edu>
2021-11-02 11:07:46 -0400

    jfc that was rough

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,21 +1,31 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed, MessageAttachment } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const { waitForDebugger } = require('inspector');
-const { Sequelize } = require('sequelize');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('changelog')
         .setDescription('Displays the current changelog message to show my users what\'s new'),
     async execute(client, message, args, Discord, interaction) {
+        guildMember = message.guild.members.cache.find(u => u.user.username === message.author.username);
         const changelogMessage = require('../changelog.json');
-        const changelogImage = new MessageAttachment('./Images/changelog.gif');
+        const messageString = JSON.stringify(changelogMessage.text).replace(/\[/gmi, "").replace(/\]/gmi, "").replace(/"/gmi, "").split(/\\n/)
         const messageEmbed = new MessageEmbed()
         .setColor("AQUA")
         .setTitle("Change Log")
-        .setDescription(`${changelogMessage.text}`)
-        .setImage('attachment://changelogImage.gif')
-        .setFooter(changelogMessage.footer.join(" "));
-        message.channel.send({embeds: [messageEmbed], files: [changelogImage]});
+        .addFields(
+            { name: "Title", value: messageString[0]},
+            {name: "Change List", value: messageString[1]},
+            {name: "UserName", value: messageString[2] },
+            {name: "UserID", value: messageString[3]},
+            {name: "CharacterName", value: messageString[4]},
+            {name: "CharacterSheet", value: messageString[5]},
+            {name: "New Commands", value: messageString[6]},
+            {name: "AllCharacters", value: messageString[7]},
+            {name: "DeleteCharacter", value: messageString[8]}
+        )
+        .setThumbnail(guildMember.displayAvatarURL({dynamic: true}))
+        .setFooter(changelogMessage.footer.join(" "))
+        message.channel.send({embeds: [messageEmbed]});
     },
 };
