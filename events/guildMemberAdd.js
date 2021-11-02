010ac195edb815437046376a2902334cb945c495
JXK1026 <JXK1026@yum.com>
2021-11-02 15:46:08 -0400

    corrected error

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,7 +8,6 @@ module.exports = {
         const welcomeChannelID = "904855083915825242"
         const ruleChannelID= "903864075065380954"
         const welcomeChannel = member.guild.channels.cache.get(welcomeChannelID)
-        welcomeChannel.send(`@${member.user.username} Hey welcome to Alawal we are all glad to see you here. To start your adventures in our world go to <#${ruleChannelID}> .
-        Check all the green check marks to continue on to the next channels till you make fully into our server.`);
+        welcomeChannel.send(`<@${member.user.id}> Hey welcome to Alawal we are all glad to see you here. To start your adventures in our world go to <#${ruleChannelID}>.\nCheck all the green check marks to continue on to the next channels till you make fully into our server.`);
     },
 };
