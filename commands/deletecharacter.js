75c36e81a06c47064a02fd977da7a1f0c274e417
JXK1026 <JXK1026@yum.com>
2021-11-02 17:18:11 -0400

    derpity derp

--- commands/deletecharacter.js
+++ commands/deletecharacter.js
@@ -32,7 +32,7 @@ module.exports = {
             const filter = (message) => {
                 return message.author === message.author
             }
-            // try{
+            try{
             const charactersAll = await Characters.findAll({ attributes: ['characterName', 'userName', 'characterSheet'], where: { characterName: name } });
             const list = charactersAll.map(c => c.userName);
             if (list.length === 1) {
@@ -72,9 +72,9 @@ module.exports = {
             } else{
                 message.reply(`Sorry, I could not find a character named: ${name[0].toUpperCase() + name.substring(1)}. Please check your spelling and try again.`)
             }
-            //     } catch {
-            //     message.reply(`Sorry, Something went catastrophically wrong. I'm probably on fire now. Do not panic, I will survive`)
-            // }
+                } catch {
+                message.reply(`Sorry, Something went catastrophically wrong. I'm probably on fire now. Do not panic, I will survive`)
+            }
         }
     }
 };
