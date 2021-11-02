3ff925b2ee68ae8bd99e5aea0533b370fb7c4c12
JXK1026 <JXK1026@yum.com>
2021-11-02 15:05:14 -0400

    i'm dumb

--- commands/deleteplayer.js
+++ commands/deleteplayer.js
@@ -21,7 +21,7 @@ const Characters = sequelize.define('characters', {
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('deletePlayer')
+        .setName('deleteplayer')
         .setDescription('Deletes a player from the database'),
     async execute(client, message, args, Discord, interaction) {
         const name = args.join(" ").toLowerCase();
