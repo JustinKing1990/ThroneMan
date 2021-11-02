a63f93408d00e2ae1f4cdbd62b73b21f7dfeb0f4
JXK1026 <JXK1026@yum.com>
2021-11-02 15:41:41 -0400

    updated to add welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,5 +5,9 @@ module.exports = {
         const config = require('../env/config.json');
         let role = member.guild.roles.cache.get('904410457304092722')
         member.roles.add(role);
+        const welcomeChannelID = "904855083915825242"
+        const welcomeChannel = member.guild.channels.cache.get(welcomeChannelID)
+        welcomeChannel.send(`@${member.user.username} Hey welcome to Alawal we are all glad to see you here. To start your adventures in our world go to rules .
+        Check all the green check marks to continue on to the next channels till you make fully into our server.`);
     },
 };
