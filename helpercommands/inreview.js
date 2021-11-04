2d6df6c7f2006ef9217d10b7be63f409033a6451
Justinking1990 <kingjust@iu.edu>
2021-11-04 10:31:55 -0400

    inreview might be good

--- helpercommands/inreview.js
+++ helpercommands/inreview.js
@@ -36,12 +36,15 @@ module.exports = {
             await thread.join();
             await thread.members.add(message.author.id)
         }
-        const staff = message.guild.roles.get('903864074134249487').members.map(m => m.user.id)
-        const dms = message.guild.roles.get('904122949940965468').members.map(m => m.user.id)
-        const membersToAdd = staff + dms
-        for(let i = 0; i< membersToAdd.length;i++){
-            thread.members.add(memberToAdd[i])
-        }
+        // const staff = message.guild.roles.cache.get('903864074134249487')
+        // const dms = message.guild.roles.cache.get('904122949940965468').members.map(m => m.user.id)
+        // const membersToAdd = staff + dms
+        // console.log(staff.members)
+        // console.log(dms)
+        // console.log(membersToAdd)
+        // // for(let i = 0; i< membersToAdd.length;i++){
+        // //     thread.members.add(membersToAdd[i])
+        // // }
         thread.send(`<@${message.author.id}><@&903864074134249487><@&904122949940965468> A new thread has been created for you to talk about the new character that ${message.author.username} made.`)
     }
 };
