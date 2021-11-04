22da1e56b26293f76a58c2d007669ef2c177abe2
Justinking1990 <kingjust@iu.edu>
2021-11-04 11:19:15 -0400

    dummy

--- helpercommands/approve.js
+++ helpercommands/approve.js
@@ -79,8 +79,7 @@ module.exports = {
         } else {
             characterListChannel.send(`User Name: ${characterPost.userName}\nCharacter Name: ${characterPost.characterName[0].toUpperCase() + characterPost.characterName.substring(1)}\nCharacter Sheet: ${characterPost.characterSheet}`);
         }
-        const channel = message.guild.channels.cache.get(message.channel)
-        console.log(channel)
+        const channel = message.guild.channels.cache.get("904144801388175470")
         const thread = await channel.threads.cache.find(x => x.name === `${message.author.username} new character}`)
         await thread.delete();
         function download(url) {
