04f07a178268d8ba507f658c0dc6cd0c2e94aea4
JustinKing1990 <kingjust@iu.edu>
2024-02-20 03:28:14 +0000

    Working on restoring denied characters better

--- buttons/confirmDeleteChannel.js
+++ buttons/confirmDeleteChannel.js
@@ -1,8 +1,25 @@
+const { getDb } = require('../mongoClient');
+const postCharacterInfo = require('../helpercommands/postCharacterInfo')
 
 module.exports = async (interaction, client) => {
+    const [action, userId, characterName, repost] = interaction.customId.split('_')
+    if (interaction.user.id !== userId) {
+        await interaction.followUp({ content: "You cannot delete another's channel this way.", ephemeral: true });
+        return;
+    }
+    const db = getDb();
+    const characterCollection = db.collection('character')
     try {
-        await interaction.channel.delete();
-        console.log("Channel deleted successfully.");
+        if (repost) {
+            const characterData = await characterCollection.findOne({ userId: interaction.user.id, name: characterName })
+            const imageUrl = characterData.imageUrls
+            await postCharacterInfo(interaction, client, characterData.name, imageUrl)
+            await interaction.channel.delete();
+        }
+        else {
+            await characterCollection.deleteOne({ userId: userId, name: characterName });
+            await interaction.channel.delete();
+        }
     } catch (error) {
         console.error('Error deleting the channel:', error);
     }
--- modals/character/characterDenialModal.js
+++ modals/character/characterDenialModal.js
@@ -2,6 +2,7 @@ const { PermissionsBitField, ActionRowBuilder, ButtonBuilder, ButtonStyle } = re
 const { getDb } = require('../../mongoClient');
 
 module.exports = async (interaction, client) => {
+    await interaction.deferReply({ ephemeral: true })
     const reason = interaction.fields.getTextInputValue('character_denial');
     const [action, userId, characterName] = interaction.customId.split("_")
     const db = getDb();
@@ -10,6 +11,25 @@ module.exports = async (interaction, client) => {
     try {
         const characterData = await charactersCollection.findOne({ userId: userId, name: characterName });
         if (characterData) {
+            let messageContent = `Character Information for ${interaction.user.username}:\n`;
+            messageContent += `Name: ${characterData.name || 'N/A'}\n`;
+            messageContent += `Title: ${characterData.title || 'N/A'}\n`;
+            messageContent += `Gender: ${characterData.gender || 'N/A'}\n`;
+            messageContent += `Age: ${characterData.age || 'N/A'}\n`;
+            messageContent += `Birthplace: ${characterData.birthplace || 'N/A'}\n`;
+            messageContent += `Height: ${characterData.height || 'N/A'}\n`;
+            messageContent += `Species: ${characterData.species || 'N/A'}\n`;
+            messageContent += `Eye Color: ${characterData.eyecolor || 'N/A'}\n`;
+            messageContent += `Hair Color: ${characterData.haircolor || 'N/A'}\n`;
+            messageContent += `Appearance: ${characterData.appearance || 'N/A'}\n`;
+            messageContent += `Weapons: ${characterData.weapons || 'N/A'}\n`;
+            messageContent += `Armor: ${characterData.armor || 'N/A'}\n`;
+            messageContent += `Beliefs: ${characterData.beliefs || 'N/A'}\n`;
+            messageContent += `Powers: ${characterData.powers || 'N/A'}\n`;
+            messageContent += `Backstory:\n`;
+            characterData.backstory.forEach((element, index) => {
+                messageContent += `${element}\n`;
+            });
             const categoryID = '905888571079139440';
             const guild = interaction.guild;
 
@@ -29,7 +49,24 @@ module.exports = async (interaction, client) => {
                 ],
             });
 
-            await privateChannel.send(`<@${interaction.user.id}> Your character submission **${characterData.name}** was denied for the following reason: ${reason}. Please adjust and resubmit.`);
+
+            let startIndex = 0;
+            const chunkSize = 1900;
+
+            while (startIndex < messageContent.length) {
+                const endIndex = Math.min(startIndex + chunkSize, messageContent.length);
+                const chunk = messageContent.substring(startIndex, endIndex);
+                const isLastChunk = endIndex >= messageContent.length;
+
+                const messageOptions = {
+                    content: chunk
+                };
+
+                const sentMessage = await privateChannel.send(messageOptions);
+
+                startIndex += chunkSize;
+            }
+            await privateChannel.send(`<@${interaction.user.id}> has denied your character submission **${characterData.name}** was denied for the following reason: ${reason}. Please adjust and resubmit.`);
 
             const confirmButton = new ActionRowBuilder()
                 .addComponents(
@@ -39,13 +76,19 @@ module.exports = async (interaction, client) => {
                         .setStyle(ButtonStyle.Danger)
                         .setEmoji('🗑️'),
                 );
+                const confirmButton2 = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId(`confirmDeleteChannel_${userId}_${characterName}_repost`)
+                        .setLabel('Confirm, Resubmit, and Delete Channel')
+                        .setStyle(ButtonStyle.Danger)
+                        .setEmoji('🗑️'),
+                );
 
             await privateChannel.send({
                 content: "Click the button below to confirm and delete this channel. This action cannot be undone.",
-                components: [confirmButton],
+                components: [confirmButton, confirmButton2],
             });
-
-            await charactersCollection.deleteOne({ userId: interaction.user.id });
 
             if (characterData.messageIds && characterData.messageIds.length > 0) {
                 const messagesToDelete = characterData.messageIds;
@@ -61,9 +104,9 @@ module.exports = async (interaction, client) => {
                 }
             }
 
-            await interaction.reply({ content: "The denial process has been completed.", ephemeral: true });
+            await interaction.editReply({ content: "The denial process has been completed.", ephemeral: true });
         } else {
-            await interaction.reply({ content: "No character data found to deny.", ephemeral: true });
+            await interaction.editReply({ content: "No character data found to deny.", ephemeral: true });
         }
     } catch (error) {
         console.error('Error processing character denial:', error);
